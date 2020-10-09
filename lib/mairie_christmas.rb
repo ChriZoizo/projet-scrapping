dfc8e5bc16e6ea112390add845fcfd3195cc95ab
ChriZoizo <71652503+ChriZoizo@users.noreply.github.com>
2020-10-09 08:43:38 +0200

    Update mairie_christmas.rb

--- lib/mairie_christmas.rb
+++ lib/mairie_christmas.rb
@@ -5,13 +5,14 @@ require 'nokogiri'
 @citys_list = Array.new
 @citys_mail = Array.new
 
+#Ici on recupere la liste des villes a partir de la page de la region et on incorpore dans une array
 def get_city_list
     url = 
     page = Nokogiri::HTML(URI.open('http://annuaire-des-mairies.com/val-d-oise.html'))
     citys = page.css('a.lientxt[href]').each {|city| @citys_list << city.text.downcase.gsub(' ', '-')}
 end
 
-
+# ici on recupere leurs emails et ont incorpore dans une array
 def get_townhall_email
     @citys_list.each do |city|
     city_page = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com/95/#{city}.html"))
@@ -19,7 +20,7 @@ def get_townhall_email
     @citys_mail << email
     end
 end
-
+# ici on fuisione les deux array
 def array_fusion
     results = Hash[*@citys_list.zip(@citys_mail).flatten]
     print results.each_slice(1).map(&:to_h)
